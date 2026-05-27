7c4d9499ad99d61313b37b6d3d59576d81b07158
bakks <pbbakkum@gmail.com>
2026-05-27 09:49:07 -0700

    Brew formula update for butterfish version v0.4.0

--- butterfish.rb
+++ butterfish.rb
@@ -5,12 +5,12 @@
 class Butterfish < Formula
   desc "A tool for using LLMs on the command line"
   homepage "https://github.com/bakks/butterfish"
-  version "0.3.13"
+  version "0.4.0"
   license "MIT"
   depends_on :macos
 
-  url "https://github.com/bakks/butterfish/releases/download/v0.3.13/butterfish_0.3.13_darwin_all.tar.gz"
-  sha256 "aa96e879348dc79bdda3a8443ed75de4f1f84f64436ad91b6a4ffb45da800233"
+  url "https://github.com/bakks/butterfish/releases/download/v0.4.0/butterfish_0.4.0_darwin_all.tar.gz"
+  sha256 "fff10cf0191489dc23d6cde8e385bdf61aa9d319d7f4dccf9c388b353263af4b"
 
   define_method(:install) do
     bin.install "butterfish"
